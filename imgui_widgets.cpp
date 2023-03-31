0f3cc84df4bb14de139507280ffa26e183472968
Lucca Troll <lucca-troll@web.de>
2023-03-31 23:15:22 +0200

    added flag to tree nodes, that hides the arrow

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -6122,7 +6122,7 @@ bool ImGui::TreeNodeBehavior(ImGuiID id, ImGuiTreeNodeFlags flags, const char* l
         RenderNavHighlight(frame_bb, id, nav_highlight_flags);
         if (flags & ImGuiTreeNodeFlags_Bullet)
             RenderBullet(window->DrawList, ImVec2(text_pos.x - text_offset_x * 0.5f, text_pos.y + g.FontSize * 0.5f), text_col);
-        else if (!is_leaf)
+        else if (!is_leaf && !(flags & ImGuiTreeNodeFlags_ArrowInvisible))
             RenderArrow(window->DrawList, ImVec2(text_pos.x - text_offset_x + padding.x, text_pos.y + g.FontSize * 0.15f), text_col, is_open ? ImGuiDir_Down : ImGuiDir_Right, 0.70f);
         if (g.LogEnabled)
             LogSetNextTextDecoration(">", NULL);
